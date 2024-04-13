8ce48197883d1e2e6497b142ba1767b413d7f6ab
maxtbuchholz <92826974+maxtbuchholz@users.noreply.github.com>
2024-04-13 14:19:29 -0500

    Initial Commit

---
+++ Sleep SoundUITests/Sleep_SoundUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Sleep_SoundUITestsLaunchTests.swift
+//  Sleep SoundUITests
+//
+//  Created by Max Buchholz on 4/13/24.
+//
+
+import XCTest
+
+final class Sleep_SoundUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
